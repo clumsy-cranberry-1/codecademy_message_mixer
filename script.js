d256c207386d499c6c434235905fa5354e7e0138
Clumsy_Cranberry_1 <mlsshrbst@gmail.com>
2022-11-08 20:56:19 +0200

    Add ingredients data

---
+++ script.js
@@ -0,0 +1,8 @@
+// PART 1
+// collection of ingredients to randomly add to create a salad
+const ingredients = {
+    fruit: ["mango", "peaches", "pineapple", "strawberries", ""],
+    vegetable: ["avocado", "cucumber", "spring onion", "red onion", "kalamata olives", "carrot", "corn", "capsicum", "tomato"],
+    meat: ["bacon", "chicken", "salmon", ""],
+    other: ["basil", "chives", "parsley", "wild rocket", "baby spinach", "feta", "mozarella", "romaine", "dried cranberries", "pecan halves", "poppy seeds", "onion flakes"],
+};
